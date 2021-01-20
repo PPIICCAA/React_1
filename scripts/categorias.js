b442e7fb5ecd7ebb2b2dc0b0705ebbf84c52f158
PPIICCAA <AbdulhaIdrees@gmail.com>
2021-01-20 13:37:16 +0100

    First

---
+++ scripts/categorias.js
@@ -0,0 +1,42 @@
+let listaCategorias = [
+
+    {
+        imgCategoria: "./img/slider/tapas.svg",
+        nombreCategoria: "Tapas",
+    },
+
+    {
+        imgCategoria: "./img/slider/breakfast.svg",
+        nombreCategoria: "Desayunos",
+    },
+
+    {
+        imgCategoria: "./img/slider/salad.png",
+        nombreCategoria: "Ensaladas",
+    },
+
+    {
+        imgCategoria: "./img/slider/fastfood.svg",
+        nombreCategoria: "Almuerzos",
+    },
+
+    {
+        imgCategoria: "./img/slider/dinner.svg",
+        nombreCategoria: "Cena",
+    },
+
+    {
+        imgCategoria: "./img/slider/desert.svg",
+        nombreCategoria: "Postres",
+    },
+
+    {
+        imgCategoria: "./img/slider/bebidas.svg",
+        nombreCategoria: "Bebidas",
+    },
+
+    {
+        imgCategoria: "./img/slider/all.svg",
+        nombreCategoria: "Todos",
+    },
+];
